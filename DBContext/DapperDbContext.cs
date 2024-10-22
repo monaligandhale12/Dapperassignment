be921efb9756fee1f56fe1bcd6049c8c4e971ec0
monaligandhale12 <119112045+monaligandhale12@users.noreply.github.com>
2024-10-22 18:06:52 +0530

    Add files via upload

---
+++ DBContext/DapperDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CurdOperationWithDapperNetCoreMVC_Demo.DBContext
+{
+    public class DapperDbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connection;
+
+        public DapperDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connection = configuration.GetConnectionString("DefaultConnection");
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_connection);
+    }
+}
